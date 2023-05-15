ac34b5a12b63753a01e1c49eda0d816c72ce0b75
Anant <105023758+AnantMishra01@users.noreply.github.com>
2023-05-15 17:38:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
-
+from flask import Flask, render_template
+import random
+app = Flask(__name__)
+@app.route('/')
+def index():
+    return render_template('index.html')
+@app.route('/roll_dice')
+def roll_dice():
+    result = random.randint(1, 6)
+    return render_template('roll_dice.html', result=result)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
